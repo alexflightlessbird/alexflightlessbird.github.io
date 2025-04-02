aa87bf15b98d6b1d6750785125cf803bf7ccc7b8
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-04-02 15:10:09 -0400

    memory card game

---
+++ memory-game/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="app">
+            <h1>Memory Card Game</h1>
+            <p>Coming Soon!</p>
+        </div>
+    )
+}
